69432ecb5a807f6cb4ecaa7caf5ca8ecde4eae6f
Nicholas Moser <nicholas.r.moser@gmail.com>
2023-03-08 17:55:09 -0700

    need to revist assignment

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,27 +1,27 @@
 async function editFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('#title').value;
-    const post_content = document.querySelector('#content').value;
-    const id = window.location.pathname.split('/')[2];
+  event.preventDefault();
 
-    const response = await fetch(`/api/post/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            title,
-            post_content,
-            id
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
+  const title = document.querySelector('#title').value;
+  const post_content = document.querySelector('#content').value;
+  const id = window.location.toString().split(`/`)[length - 1
+];
+
+  const response = await fetch(`/api/post/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title,
+      post_content,
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+}
+
+document.querySelector('.edit-post-btn').addEventListener('click', editFormHandler);
