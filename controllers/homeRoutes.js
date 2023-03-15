d105ecbf30d39b23d3b2787d0f2642762393aa45
Nicholas Moser <nicholas.r.moser@gmail.com>
2023-03-15 14:16:23 -0600

    signUp

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,7 +28,7 @@ router.get('/login', async (req, res) => {
     
 });
 
-router.get('/signUp', async (req, res) => {
+router.get('/signup', async (req, res) => {
     console.log("here")
 
         res.render('signUp', {
